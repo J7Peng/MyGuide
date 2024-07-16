295fca91bd597d9b575b6115ecc07d2f513e5298
J7Peng <3430227607@qq.com>
2024-07-16 13:37:49 +0800

    setText

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -174,7 +174,7 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::updateAnnouncementTextEdit() {
-    ui->anouncementtextEdit->append(*changes);
+    ui->anouncementtextEdit->setText(*changes);
 }
 
 void MainWindow::changeBackground() {
@@ -223,7 +223,7 @@ void MainWindow::changeBackground() {
 void MainWindow::openTouristWindow()
 {
     touristWindow = new TouristWindow(graph, this);
-    securityWindow->setAttribute(Qt::WA_DeleteOnClose);
+    touristWindow->setAttribute(Qt::WA_DeleteOnClose);
     touristWindow->show();
 }
 
